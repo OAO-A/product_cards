d38441f10f1475ee2481e0baa5e938d41097a235
Andrii Stiebalov <stiebalov@gmail.com>
2022-10-30 17:46:45 +0200

    server was added

---
+++ product_catalog-BackEnd/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const sequelize = require('./db');
+
+const app = express();
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start;
