b8a57f376a963d47d7397277160a07675109a1b5
Anthony Buhaienko <dubsterok@gmail.com>
2022-10-30 21:57:50 +0200

    Footer added

--- product_catalog-FrontEnd/src/components/Footer/Footer.tsx
+++ product_catalog-FrontEnd/src/components/Footer/Footer.tsx
@@ -3,10 +3,15 @@ import style from './footer.module.scss';
 
 export const Footer: React.FC = () => {
   return <footer className={`${style.footer}`}>
-    <a href='/' className={`${style.footer_logo}`}></a>
-
-    <a href='/' className={`${style.footer_nav}`}>github</a>
-    <a href='/' className={`${style.footer_nav}`}>contacts</a>
-    <a href='/' className={`${style.footer_nav}`}>rights</a>
+    <a href='/' >
+      <img src="Logo.png" id='top' className={`${style.footer_logo}`} alt="/" />
+    </a>
+    <div className={`${style.footer_nav}`}>
+      <a href='/' className={`${style.footer_nav}`}>github</a>
+      <a href='/' className={`${style.footer_nav}`}>contacts</a>
+      <a href='/' className={`${style.footer_nav}`}>rights</a>
+      <a href="" className={`${style.footer_nav}`} hidden></a>
+    </div>
+    <a href='top' className={`${style.footer_nav_last}`}>Back to stop</a>
   </footer>;
 };
