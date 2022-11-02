4d788ec59287cd19aad1632b2b8182838768af78
Anthony Buhaienko <dubsterok@gmail.com>
2022-11-02 17:40:02 +0200

    footer links fixed

--- product_catalog-FrontEnd/src/components/Footer/Footer.tsx
+++ product_catalog-FrontEnd/src/components/Footer/Footer.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import style from './footer.module.scss';
+import { Link, NavLink } from 'react-router-dom';
+
 import logo from '../../img/Logo.png';
 
 export const Footer: React.FC = () => {
   return (
     <footer className={`${style.footer}`}>
-      <a href="/">
+      <Link to="/">
         <img
           src={logo}
           id="top"
           className={`${style.footer_logo}`}
           alt="nice-gadget"
         />
-      </a>
+      </Link>
       <div className={`${style.footer_nav}`}>
-        <a href="/github" className={`${style.footer_nav}`}>
+        <NavLink to="/github" className={`${style.footer_nav}`}>
           github
-        </a>
-        <a href="/contnacts" className={`${style.footer_nav}`}>
+        </NavLink>
+        <NavLink to="/contnacts" className={`${style.footer_nav}`}>
           contacts
-        </a>
-        <a href="/rights" className={`${style.footer_nav}`}>
+        </NavLink>
+        <NavLink to="/rights" className={`${style.footer_nav}`}>
           rights
-        </a>
-        <a href="" className={`${style.footer_nav}`} hidden></a>
+        </NavLink>
+        <NavLink to="" className={`${style.footer_nav}`} hidden></NavLink>
       </div>
-      <a href="#" className={`${style.footer_nav_last}`}>
+      <NavLink to="#" className={`${style.footer_nav_last}`}>
         Back to stop
-      </a>
+      </NavLink>
     </footer>
   );
 };
