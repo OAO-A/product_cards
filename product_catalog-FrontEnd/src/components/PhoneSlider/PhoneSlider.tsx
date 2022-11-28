1e06ad918b1e5974a6bdaadbad9d13b519fce0a5
Anthony Buhaienko <dubsterok@gmail.com>
2022-11-28 17:04:16 +0200

    refactored Shop by category block + fixed a bit swiper width

--- product_catalog-FrontEnd/src/components/PhoneSlider/PhoneSlider.tsx
+++ product_catalog-FrontEnd/src/components/PhoneSlider/PhoneSlider.tsx
@@ -13,11 +13,11 @@ export const PhoneSlider: React.FC<Props> = ({ phones }) => {
   const showPhones = phones.slice(1, 9);
 
   return (
-    <div className={`${Styles.container} S--1-4 T--1-12 D--1-24`}>
+    <div className={`${Styles.container} S--1-4 T--1-11 D--1-24`}>
       <Swiper
         modules={[Navigation, Pagination]}
-        spaceBetween={20}
-        slidesPerView={1.4}
+        spaceBetween={15}
+        slidesPerView='auto'
         breakpoints={{
           640: { slidesPerView: 2.5 },
           1200: { slidesPerView: 4 },
@@ -26,7 +26,7 @@ export const PhoneSlider: React.FC<Props> = ({ phones }) => {
           prevEl: '.home__buttonprev',
           nextEl: '.home__buttonnext',
         }}
-        loop
+        loop={true}
       >
         {showPhones.map((phone) => (
           <SwiperSlide key={phone.id}>
--- product_catalog-FrontEnd/src/pages/HomePage/HomePage.tsx
+++ product_catalog-FrontEnd/src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,6 @@ import { PhoneSlider } from '../../components/PhoneSlider';
 import { Slider } from '../../components/Slider';
 import { getDevices } from '../../Utils/interface/fetchClient';
 import { NavLink } from 'react-router-dom';
-import { Category } from '../../components/Categories/Category';
 import banner1 from './banners/category-phones.png';
 import banner2 from './banners/category-tablets.png';
 import banner3 from './banners/category-accessories.png';
@@ -13,6 +12,8 @@ import banner3 from './banners/category-accessories.png';
 export const HomePage: React.FC = () => {
   const [devices, setDevices] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const hotPrices = devices.filter(devices => devices.priceDiscount > 1200);
   
   const getDevicesFromServer = async () => {
     try {
@@ -45,35 +46,32 @@ export const HomePage: React.FC = () => {
         <div className="home__buttonprev"></div>
         <div className="home__buttonnext"></div>
       </div>
-      <PhoneSlider phones={devices} />
+      <PhoneSlider phones={hotPrices} />
 
       <h2 className={`${Style.home__subtitle} ${Style.home__subtitlecategory} S--1-3 T--1-9 D--1-10`}>
         Shop by category
       </h2>
       <div className={`${Style.home__categories} ${Style.categories} S--1-4 T--1-12 D--1-24`}>
         <NavLink to={'/phones'} className={`${Style.categories__block} S--1-4 T--1-4 D--1-4`}>
-          <Category
-            image={banner1}
-            title={'Phones'}
-            text={'95 models'}
-            backgroundColor={`${Style.categories__wrapper} ${Style.categories__wrappermobile}`}
-          />
+          <div className={Style.category__backgroundPhones}>
+            <img src={banner1} alt="phones" className={Style.category__imagePhone}/>
+          </div>
+          <h4 className={Style.category__title}>Mobile phones</h4>
+          <p className={Style.category__text}>95 models</p>
         </NavLink>
         <NavLink to={'/tablets'} className={`${Style.categories__block} S--1-4 T--1-4 D--5-8`}>
-          <Category
-            image={banner2}
-            title={'Tablets'}
-            text={'15 models'}
-            backgroundColor={`${Style.categories__wrapper} ${Style.categories__wrappertablet}`}
-          />
+          <div className={Style.category__backgroundTablets}>
+            <img src={banner2} alt="tablets" className={Style.category__imageTablet}/>
+          </div>
+          <h4 className={Style.category__title}>Tablets</h4>
+          <p className={Style.category__text}>24 models</p>
         </NavLink>
         <NavLink to={'/accessories'} className={`${Style.categories__block} S--1-4 T--1-4 D--5-4`}>
-          <Category
-            image={banner3}
-            title={'Accessories'}
-            text={'117 models'}
-            backgroundColor={`${Style.categories__wrapper} ${Style.categories__wrapperaccessories}`}
-          />
+          <div className={Style.category__backgroundAccessories}>
+            <img src={banner3} alt="accessories" className={` ${Style.category__imageAcc}`}/>
+          </div>
+          <h4 className={Style.category__title}>Accessories</h4>
+          <p className={Style.category__text}>100 models</p>
         </NavLink>
       </div>
       <h2 className={`${Style.home__subtitle} S--1-3 T--1-9 D--1-10`}>
